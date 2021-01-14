c1a04475e3e705744c6af7a9872dca98ff8bde09
Farrukh Khan <farrukh_mask@hotmail.com>
2021-01-14 16:04:31 +0000

    Loan App Backend

---
+++ ASPNETCoreAndMediatR/ExampleMediatR/Dtos/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleMediatR.Dtos
+{
+    public class TestClass
+    {
+        public string name { get; set; }
+        public string secName { get; set; }
+    }
+}
